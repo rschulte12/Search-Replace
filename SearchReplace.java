1a4c59e6548a17bc213af1a5ba49f8b1228959e7
artjgomez <artjgomez@outlook.com>
2020-02-08 17:06:15 -0600

    Comments for each step

--- SearchReplace.java
+++ SearchReplace.java
@@ -2,7 +2,15 @@
 
 public class SearchReplace {
 	public static void main(String[] args) {
-		
+		// Ask the user for file name
+		// Ask user if they want to see word count or perform search and replace
+		// Word count use case parses file using empty spaces (space, tab, etc.) as delimiters
+		// Word count then displays each unique word followed by its count. Sorted most used to least used words.
+		// Search and replace asks the user what word to search for
+		// Search and replace performs a silent word count (to populate location arrays)
+		// Search and replace displays the number of times the searched word appears in the file
+		// Search and replace asks the user what they want to replace the word with
+		// Search and replace goes to the last known location first to start the replacements and continues backward (so the location values don't have to change)
 	}
 }
 
