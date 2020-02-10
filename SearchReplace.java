b05c32f4e5b34cb4309ce1ac0fa48a15e400c864
artjgomez <artjgomez@outlook.com>
2020-02-10 11:38:34 -0600

    Partial Implementation

--- SearchReplace.java
+++ SearchReplace.java
@@ -1,9 +1,37 @@
 
+import java.util.Scanner;
 
 public class SearchReplace {
 	public static void main(String[] args) {
-		// Ask the user for file name
-		// Ask user if they want to see word count or perform search and replace
+		String file, sWord, rWord;
+		int choice = 2;
+		Scanner userIn = new Scanner(System.in);
+		System.out.println("Enter name of the file: ");
+		file = userIn.next();
+		
+		System.out.println("Choose an operation: (0 or 1) \nShow word count [0]\nSearch and replace a word [1]");
+		choice = userIn.nextInt();
+		wordcount newWC = wordcount(file);
+		
+		if (choice == 0) {
+			newWC.print(1);
+		}
+		else if (choice == 1) {
+			
+			
+			
+			replace newReplace = replace(newWC); // create new replace object for replace operations
+																// constructor displays the number of times the search word is in the file
+			newReplace.overWrite(rWord);
+		
+		}
+		else {
+			
+		}
+		
+		
+		// * Ask the user for file name
+		// * Ask user if they want to see word count or perform search and replace
 		// Word count use case parses file using empty spaces (space, tab, etc.) as delimiters
 		// Word count then displays each unique word followed by its count. Sorted most used to least used words.
 		// Search and replace asks the user what word to search for
---
+++ replace.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class replace {
+	private String sWord, rWord;
+	private Scanner userIn = new Scanner(System.in);
+	
+	
+	public replace(wordcount) {
+		System.out.println("Enter a word to search for: ");
+		sWord = userIn.next();
+		System.out.println("Enter a replacement word: ");
+		rWord = userIn.next();
+	}
+	// Main program passes in file name, word to search, and word to replace with
+	// Has method that determines if the file or word exists
+	// Has method that replaces the words and creates in file object
+	// Has method that writes the file object to an external text file the replace the original file
+	
+	// values: wordcount, sWord, rWord, 
+	// methods: overWrite
+	
+	
+	
+	
+}
