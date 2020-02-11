0b889e935d44f27479cd21e94b288b43cdecf651
rschulte12 <41649489+rschulte12@users.noreply.github.com>
2020-02-10 20:18:20 -0600

    Update wordcount.java

---
+++ wordcount.java
@@ -0,0 +1,9 @@
+class wordCount {
+    String word;
+    Set<Integer> positions = new HashSet<>();
+    
+    public wordCount(String w, int p) {
+        word = w;
+        positions.add(p);
+    }
+}
