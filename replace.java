79fdb32278f8965bc23cfc06027b0d78898a32e2
artjgomez <artjgomez@outlook.com>
2020-02-13 21:40:20 -0600

    Initial Implementation

--- replace.java
+++ replace.java
@@ -1,16 +1,119 @@
 import java.util.Scanner;
 
 public class replace {
-	private String sWord, rWord;
+	
+	private String sWord, rWord, fileName;
+	private Scanner userIn = new Scanner(System.in);
+	
+	
+	public replace(String fileName) {
+		this.fileName = fileName;
+		System.out.println("Enter a word to search for: ");
+		sWord = userIn.next();
+		System.out.println("Enter a replacement word: ");
+		rWord = userIn.next();
+	
+		File f=new File(fileName);
+        
+        FileInputStream fs = null;
+        InputStreamReader in = null;
+        BufferedReader br = null;
+        
+        StringBuffer sb = new StringBuffer();
+        
+        String textinLine;
+        
+        try {
+             fs = new FileInputStream(f);
+             in = new InputStreamReader(fs);
+             br = new BufferedReader(in);
+            
+            while(true)
+            {
+                textinLine=br.readLine();
+                if(textinLine==null)
+                    break;
+                sb.append(textinLine + "\n");
+				try {
+					while (true) {
+						String textToEdit1 = "abc";
+						int cnt1 = sb.indexOf(textToEdit1);
+						sb.replace(cnt1,cnt1+textToEdit1.length(),"REPLACED");
+					}
+				}
+				catch (Exception e) {
+				}
+            }
+			
+              fs.close();
+              in.close();
+              br.close();
+
+            } catch (FileNotFoundException e) {
+              e.printStackTrace();
+            } catch (IOException e) {
+              e.printStackTrace();
+            }
+            
+            try{
+                FileWriter fstream = new FileWriter(f);
+                BufferedWriter outobj = new BufferedWriter(fstream);
+                outobj.write(sb.toString());
+                outobj.close();
+                
+            }catch (Exception e){
+              System.err.println("Error: " + e.getMessage());
+            }
+	}
+}
+	
+	
+	
+	
+	
+	
+	
+	
+	/* private String sWord, rWord;
 	private Scanner userIn = new Scanner(System.in);
 	
 	
 	public replace(wordcount) {
 		System.out.println("Enter a word to search for: ");
 		sWord = userIn.next();
-		System.out.println("Enter a replacement word: ");
-		rWord = userIn.next();
+		
+		if (find(sWord)) {
+			System.out.println("Enter a replacement word: ");
+			rWord = userIn.next();
+			
+			
+			
+			
+			
+			
+			
+		}
+		else {
+			System.out.println("That word doesn't exist in the file.");
+		}
 	}
+	
+	public boolean find(String sWord) { // Returns true if sWord exists in the wordcount list, false if it doesn't
+		
+		for (String foundWord : wc) { // For each word in the wordcount list
+			if (foundWord.equals(sWord)) { // Compare the list word to the searched word
+				return true; // Return true if a match is found
+			}
+		}
+		return false; // Return false if a match was never found
+	}
+	
+	
+	 */
+	
+	
+	
+	
 	// Main program passes in file name, word to search, and word to replace with
 	// Has method that determines if the file or word exists
 	// Has method that replaces the words and creates in file object
@@ -18,8 +121,3 @@ public class replace {
 	
 	// values: wordcount, sWord, rWord, 
 	// methods: overWrite
-	
-	
-	
-	
-}
